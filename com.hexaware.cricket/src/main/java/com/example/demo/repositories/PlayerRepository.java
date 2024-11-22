bebf7fe5a6ee78fa0adb9517a13a54013d75d225
laxmanndhotre <117575876+laxmanndhotre@users.noreply.github.com>
2024-11-22 11:53:07 +0530

    pushing the code challenge project

---
+++ com.hexaware.cricket/src/main/java/com/example/demo/repositories/PlayerRepository.java
@@ -0,0 +1,14 @@
+package com.example.demo.repositories;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.example.demo.entities.Player;
+
+public interface PlayerRepository extends JpaRepository<Player, Integer>{
+	
+
+}
---
+++ com.hexaware.cricket/src/main/java/com/example/demo/services/IPlayerService.java
@@ -0,0 +1,18 @@
+package com.example.demo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.example.demo.entities.Player;
+import com.example.demo.exceptions.PlayerNotFoundException;
+
+public interface IPlayerService {
+	
+	    List<Player> getAllPlayers();
+	    Optional<Player> getPlayerById(Integer id);
+	    Player addPlayer(Player player);
+	    Player updatePlayer(Integer id, Player newPlayer) throws PlayerNotFoundException;
+	    void deletePlayer(Integer id);
+	
+
+}
